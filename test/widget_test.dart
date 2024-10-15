8099f401a09175f0346feb992e22272dc4620f02
prathameshkhade <pkhade2865@gmail.com>
2024-10-15 20:11:26 +0530

    rm redundant code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:sysadmin/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Hello());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
