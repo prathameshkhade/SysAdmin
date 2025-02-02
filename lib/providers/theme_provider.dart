e7e585b481608b5f4dde0a111ed4b5fe4754d5fb
prathameshkhade <pkhade2865@gmail.com>
2025-02-02 17:45:06 +0530

    add: themeProvider
    using riverpod, you can get and toggle the theme

---
+++ lib/providers/theme_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final themeProvider = StateNotifierProvider<ThemeNotifier, bool>((ref) {
+  return ThemeNotifier();
+});
+
+class ThemeNotifier extends StateNotifier<bool> {
+  ThemeNotifier() : super(false); // Start with light theme
+
+  void toggleTheme() {
+    state = !state;
+  }
+}
